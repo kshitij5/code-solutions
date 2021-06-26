88a72dbfb4724164fb1c7603f38eba18a93f58e6
kshitij5 <kshitijliveat5@gmail.com>
2021-06-27 01:04:33 +0530

    day 26 code added

---
+++ LEETCODE/JUNE CHALLENGE/day26_count_smaller_number.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    //brute force
+    // vector<int> countSmaller(vector<int>& nums) {
+        
+    //     vector<int> counts;
+    //     for (int i = 0; i < nums.size(); i++) {
+    //         int lessCount = 0;
+    //         for (int j = i+1; j < nums.size(); j++) {
+    //             if(nums[i]>nums[j]) lessCount++;
+    //         }
+
+    //         counts.push_back(lessCount);
+    //     }
+
+    //     return counts;
+    // }
+
+    //using dp and optimizattion
+    vector<int> countSmaller(vector<int>& nums) {
+        
+    }
+};
