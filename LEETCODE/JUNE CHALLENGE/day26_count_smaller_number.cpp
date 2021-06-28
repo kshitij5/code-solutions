5a1110d74db3c6718c5db21af9ce25ef40a3582d
kshitij5 <kshitijliveat5@gmail.com>
2021-06-28 09:57:09 +0530

    day 26 code updated

--- LEETCODE/JUNE CHALLENGE/day26_count_smaller_number.cpp
+++ LEETCODE/JUNE CHALLENGE/day26_count_smaller_number.cpp
@@ -19,8 +19,34 @@ public:
     //     return counts;
     // }
 
-    //using dp and optimizattion
+    //USING Binary Indexed Tree 
+    //Explaination here: 
+    //https://leetcode.com/problems/count-of-smaller-numbers-after-self/discuss/1298226/Easy-Solution-w-Explanation-or-Segment-Tree-and-Binary-Indexed-Tree-Approach
+    int bit[100001];
+    int offset=1e4+1;
+    int n;
+    
+    void update(int index){
+        for(;index<=100000;index+=index&(-index))
+            bit[index]++;
+    }
+    
+    int query(int index){
+        int count=0;
+        for(;index>0;index-=index&(-index))
+            count+=bit[index];
+        return count;
+    }
+    
     vector<int> countSmaller(vector<int>& nums) {
+        n=nums.size();
+        vector<int> ans(n);
         
+        for(int i=n-1;i>=0;i--){
+            ans[i]=query(nums[i]+offset-1);
+            update(nums[i]+offset);
+        }
+        
+        return ans;
     }
 };
