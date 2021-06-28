60d4c862ea5e84c50ba2f1fe9720be898c4ba3f4
kshitij5 <kshitijliveat5@gmail.com>
2021-06-28 09:54:37 +0530

    code updated

--- CODECHEF/LTIME97C/TICTACTOE.cpp
+++ CODECHEF/LTIME97C/TICTACTOE.cpp
@@ -10,7 +10,7 @@ signed main(){
 //    ios::sync_with_stdio(0);
 //   cin.tie(0); cout.tie(0);
 
-    int t;  //no of test cases
+    int t;  //no of test cases 
     cin >> t;
 
     while (t--){
--- CODECHEF/LTIME97C/tempCodeRunnerFile.cpp
+++ CODECHEF/LTIME97C/tempCodeRunnerFile.cpp
@@ -1,18 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pii pair<int, int>
+#define mod 1000000007
+#define endl '\n'
+
 signed main(){
 //    ios::sync_with_stdio(0);
 //   cin.tie(0); cout.tie(0);
 
-    int t;  //no of test cases
-    cin >> t;
+    int n, q;
+    cin >> n >> q;
+    
+    int seq[n+1];
 
-    while (t--){
-        int a, b;
-        cin >> a >> b;
+    for(int i = 1; i < n+1; i++) {
+        cin >> seq[i];
+    }
 
-        if(b&(-b)) {
-            cout << "YES" << endl;
+    int l, r, x, y;
+    while(q--) {
+        int temp;
+        cin >> temp;
+
+        if(temp==1) {
+            cin >> l >> r >> x ;
+            for (int i = l; i <= r ; i++) {
+                seq[i] += (x+i-l)*(x+i-l);
+            }
         }else{
-            cout << "NO" << endl;
+            cin >> y;
+            cout << seq[y] << endl;
         }
     }
     return 0;
